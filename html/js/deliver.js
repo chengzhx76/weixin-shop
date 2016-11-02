cf446e9f9c81eaebee541170d2b4d1c0138572b7
chengzhx76 <chengzhx76@qq.com>
2016-11-02 13:53:26 +0800

    送货地址

--- html/js/deliver.js
+++ html/js/deliver.js
@@ -6,17 +6,103 @@ $(function () {
     $('section').hide();
     $(".weui_navbar .weui_navbar_item:first").addClass("tab-red").show();
     $("section:first").show();
+
+    $('#net-loading').show();
+    //jsonp模式：进入该页，请求数据
+    ajaxHttpRequest('user/v1/all/addr', {
+        jsonpCallback: 'handler',
+        success: function (data, status) {
+            // 请求出现异常
+            if (status != "success") {
+                showError("请求出现异常！");
+                $('#net-loading').hide();
+                return;
+            }
+            $('#net-loading').hide();
+        },
+        error: function (errorType, error) {
+            showError("ERROR--请求出现异常！");
+            $('#net-loading').hide();
+        }
+    });
+});
+
+function handler(data) {
+    if (!data.meta.success) {
+        showError(data.meta.msg);
+        $('#net-loading').hide();
+        return;
+    }
+    var home = template('home-temp', data);
+    $('.home-wrap').html(home);
+
     $(".weui_navbar_item").click(function() {
         $(".weui_navbar_item").removeClass("tab-red");
         $(this).addClass("tab-red");
         $("section").hide();
         var activeTab = $(this).find("a").attr("href");
-        if(activeTab == '#since') {
-            $('.footer').hide();
-        }else {
+        $('#net-loading').show();
+
+        if(activeTab == '#home') {
+            ajaxHttpRequest('user/v1/all/addr', {
+                jsonpCallback: 'homeHandler',
+                success: function (data, status) {
+                    // 请求出现异常
+                    if (status != "success") {
+                        showError("请求出现异常！");
+                        $('#net-loading').hide();
+                        return;
+                    }
+                    $('#net-loading').hide();
+                },
+                error: function (errorType, error) {
+                    showError("ERROR--请求出现异常！");
+                    $('#net-loading').hide();
+                }
+            });
+
             $('.footer').show();
+        }else {
+            ajaxHttpRequest('user/v1/all/addr', {
+                jsonpCallback: 'sinceHandler',
+                success: function (data, status) {
+                    // 请求出现异常
+                    if (status != "success") {
+                        showError("请求出现异常！");
+                        $('#net-loading').hide();
+                        return;
+                    }
+                    $('#net-loading').hide();
+                },
+                error: function (errorType, error) {
+                    showError("ERROR--请求出现异常！");
+                    $('#net-loading').hide();
+                }
+            });
+
+            $('.footer').hide();
         }
         $(activeTab).fadeIn();
         return false;
     });
-});
+}
+
+function homeHandler(data) {
+    if (!data.meta.success) {
+        showError(data.meta.msg);
+        $('#net-loading').hide();
+        return;
+    }
+    var home = template('home-temp', data);
+    $('.home-wrap').html(home);
+}
+
+function sinceHandler(data) {
+    if (!data.meta.success) {
+        showError(data.meta.msg);
+        $('#net-loading').hide();
+        return;
+    }
+    var since = template('since-temp', data);
+    $('.since-wrap').html(since);
+}
