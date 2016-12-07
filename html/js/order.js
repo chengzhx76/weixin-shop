53dd2ac99c1fb2904df044d8a9a8d0f33f88df17
chengzhx76 <chengzhx76@qq.com>
2016-12-07 13:46:24 +0800

    分页bug修复

--- html/js/order.js
+++ html/js/order.js
@@ -1,5 +1,6 @@
 var page = 1;
 $(function () {
+    setProductTotalPrice();
     var $totalPrice = $(".total-price").children("strong").text();
     if($totalPrice!="" && $totalPrice!="0") {
         $(".total-price").show();
